e8a4f750dcf7354e02f57e17f518a2afb74c9087
Rafael Ramirez <rafa.yael@gmail.com>
2021-12-09 10:36:47 -0600

    fix: Error en package.json

---
+++ server.js
@@ -0,0 +1,16 @@
+//Install express server
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist"));
+
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname + "/dist/index.html"))
+);
+
+// Start the app by listening on the default Heroku port
+const PORT = process.env.PORT || 5000;
+console.log(PORT);
+app.listen(PORT);
