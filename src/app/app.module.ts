39d786f720c77090dcd370927bfaea69ec7c8fc5
Rafael Ramirez <rafa.yael@gmail.com>
2021-12-09 13:23:39 -0600

    feat: pwa agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UrlBaseInterceptor } from './interceptors/url-base.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PagesModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: UrlBaseInterceptor, multi: true },
   ],
